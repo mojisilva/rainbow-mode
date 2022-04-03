0516ba88f727724f5b89b369087f3a609fec2e8c
mojisilva <mojiasilva@gmail.com>
2022-04-03 19:28:30 -0300

    updating script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-const colorfullMode = 'colorfull-mode';
+const rainbowModeClass = 'colorfull-mode';
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
 const body = document.getElementsByTagName('body')[0];
@@ -15,19 +15,32 @@ button.addEventListener('click', changeMode);
 
 
 function changeClasses(){
-    button.classList.toggle(colorfullMode);
-    divRed.classList.toggle(colorfullMode);
-    divOrange.classList.toggle(colorfullMode);
-    divYellow.classList.toggle(colorfullMode);
-    divGreen.classList.toggle(colorfullMode);
-    divBlue.classList.toggle(colorfullMode);
-    divIndigo.classList.toggle(colorfullMode);
-    divViolet.classList.toggle(colorfullMode);
-    body.classList.toggle(colorfullMode);
-    footer.classList.toggle(colorfullMode);
+    button.classList.toggle(rainbowModeClass);
+    divRed.classList.toggle(rainbowModeClass);
+    divOrange.classList.toggle(rainbowModeClass);
+    divYellow.classList.toggle(rainbowModeClass);
+    divGreen.classList.toggle(rainbowModeClass);
+    divBlue.classList.toggle(rainbowModeClass);
+    divIndigo.classList.toggle(rainbowModeClass);
+    divViolet.classList.toggle(rainbowModeClass);
+    body.classList.toggle(rainbowModeClass);
+    footer.classList.toggle(rainbowModeClass);
 }
 
+function changeText(){
+    const lightMode = "Light Mode";
+    const rainbowMode = "Rainbow Mode";
+    if(body.classList.contains(rainbowModeClass)){
+        button.innerHTML = lightMode;
+        h1.innerHTML = `${rainbowMode} ON`;
+        return;
+    }
+    button.innerHTML = rainbowMode;
+    h1.innerHTML = `${lightMode} ON`; 
+
+}
 function changeMode(){
     changeClasses();
+    changeText();
 }
 
